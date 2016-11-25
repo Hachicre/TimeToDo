1cbb55cb46509f8b24b8bcaa1ca11388b8973d22
NozomiSugiyama <nontan4061@gmail.com>
2016-11-25 09:46:35 +0900

    [Update] TimeTo.java, Plane.java

--- app/src/main/java/com/example/nozomisugiyama/timetodo/MainActivity.java
+++ app/src/main/java/com/example/nozomisugiyama/timetodo/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends AppCompatActivity
         YearFragment.OnFragmentInteractionListener,
         TopFragment.OnFragmentInteractionListener {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
